b12289d1cdf234eed9e392d7d1f0bc9833b91d9c
up1 <somkiat.p@gmail.com>
2019-08-21 14:43:28 +0700

    Start project

---
+++ src/main/java/com/example/workshophelloservice/WorkshopHelloServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshophelloservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopHelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopHelloServiceApplication.class, args);
+	}
+
+}
