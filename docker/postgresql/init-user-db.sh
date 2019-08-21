65cddd62e84299be77b3919cc61b612677fdfc71
up1 <somkiat.p@gmail.com>
2019-08-21 15:55:36 +0700

    Working with docker

--- docker/postgresql/init-user-db.sh
+++ docker/postgresql/init-user-db.sh
@@ -4,4 +4,13 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE messagedb;
     GRANT ALL PRIVILEGES ON DATABASE messagedb TO dbuser;
+    \l
+    \c messagedb
+    CREATE TABLE message(
+       id serial PRIMARY KEY,
+       data VARCHAR (50) UNIQUE NOT NULL
+    );
+
+    INSERT INTO message VALUES (1, 'Hello World');
 EOSQL
+
