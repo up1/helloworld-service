71dc3c850a9f84cc9e27865966a4a0f15b616702
up1 <somkiat.p@gmail.com>
2019-08-21 17:32:07 +0700

    Update docker file

--- docker/postgresql/init-user-db.sh
+++ docker/postgresql/init-user-db.sh
@@ -11,6 +11,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
        data VARCHAR (50) UNIQUE NOT NULL
     );
 
-    INSERT INTO message VALUES (1, 'Hello World');
+    INSERT INTO message VALUES (1, 'Hello World 2');
 EOSQL
 
