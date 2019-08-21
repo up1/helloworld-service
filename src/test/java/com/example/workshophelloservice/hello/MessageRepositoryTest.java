627146353ddfdcc8dcddff27a0b726c5c5b7a54a
up1 <somkiat.p@gmail.com>
2019-08-21 23:10:48 +0700

    Working with repository testing

---
+++ src/test/java/com/example/workshophelloservice/hello/MessageRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.example.workshophelloservice.hello;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class MessageRepositoryTest {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @Before
+    public void initData() {
+        Message message = new Message();
+        message.setId(1);
+        message.setData("Hello World");
+        messageRepository.save(message);
+    }
+
+    @Test
+    public void found_message_with_get_data_by_id() {
+        Optional<Message> optionalMessage = messageRepository.findById(1);
+        String message = optionalMessage.get().getData();
+        assertEquals("Hello World", message);
+    }
+
+}
