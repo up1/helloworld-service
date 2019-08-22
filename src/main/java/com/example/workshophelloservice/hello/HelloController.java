851cfa6e65235ad1505666257af7d423c10b6a7b
up1 <somkiat.p@gmail.com>
2019-08-22 09:08:54 +0700

    Working with centralize logging with ELK stack

--- src/main/java/com/example/workshophelloservice/hello/HelloController.java
+++ src/main/java/com/example/workshophelloservice/hello/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.workshophelloservice.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +11,20 @@ import java.util.Optional;
 @RestController
 public class HelloController {
 
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @Autowired
     private MessageRepository messageRepository;
 
     @GetMapping("/hi")
     public HelloResponse hi() {
+        log.info("Start to say hi");
+
         Optional<Message> optionalMessage = messageRepository.findById(1);
         String message = optionalMessage.get().getData();
+
+        log.info("Say hi with message : {}" , message);
+
         return new HelloResponse(message);
     }
 
