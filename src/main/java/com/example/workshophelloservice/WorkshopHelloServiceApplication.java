19c8299a28ee9c49963c059ba32aed546219d850
up1 <somkiat.p@gmail.com>
2019-09-13 09:05:03 +0700

    Demo with tracing and logging

--- src/main/java/com/example/workshophelloservice/WorkshopHelloServiceApplication.java
+++ src/main/java/com/example/workshophelloservice/WorkshopHelloServiceApplication.java
@@ -2,12 +2,20 @@ package com.example.workshophelloservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 public class WorkshopHelloServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkshopHelloServiceApplication.class, args);
+		ConfigurableApplicationContext ctx =  SpringApplication.run(WorkshopHelloServiceApplication.class, args);
+		String[] beans = ctx.getBeanDefinitionNames();
+		for (String bean: beans) {
+			System.out.println(bean);
+		}
+		System.out.println(">>>" + ctx.getBeanDefinitionCount());
 	}
 
 }
--- src/main/java/com/example/workshophelloservice/hello/HelloControlller.java
+++ src/main/java/com/example/workshophelloservice/hello/HelloControlller.java
@@ -1,5 +1,7 @@
 package com.example.workshophelloservice.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,14 @@ import java.util.Optional;
 @RestController
 public class HelloControlller {
 
+    private static final Logger log = LoggerFactory.getLogger(HelloControlller.class);
+
     @Autowired
     private HelloService helloService;
 
     @GetMapping("/hi")
     public HelloResponse hi() {
+        log.info("Start to say hi");
         return helloService.process();
     }
 
